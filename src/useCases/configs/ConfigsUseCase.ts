1b8044cba093eb9cc6559d2dbc46f3183a8ad2e7
Rodrigo Daniel <Rodrigodaniel1955@gmail.com>
2022-06-29 04:35:54 +0000

    first commit

---
+++ src/useCases/configs/ConfigsUseCase.ts
@@ -0,0 +1,52 @@
+import { client } from "../../prisma/client"
+
+interface IConfigsRequest {
+    id?: string
+    name?: string
+    standard?: number
+    password?: string
+    activo?: boolean
+}
+
+class ConfigsUseCase {
+    async create({ name, standard, password, activo }: IConfigsRequest) {
+        const configs = await client.config.create({
+            data: {
+                name,
+                standard,
+                password,
+                activo
+            }
+        })
+
+        return configs
+    }
+
+    async show() {
+        const configs = await client.config.findFirst()
+
+        return configs
+    }
+
+    async findByName(name: string) {
+        const config = await client.config.findFirst({
+            where: { name }
+        })
+
+        return config
+    }
+
+    async update({ standard, activo, id }: IConfigsRequest) {
+        const configs = await client.config.update({
+            where: { id },
+            data: {
+                standard,
+                activo
+            }
+        })
+
+        return configs
+    }
+}
+
+export { ConfigsUseCase }
